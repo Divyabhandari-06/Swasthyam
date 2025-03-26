096c61abe382c4f51791a1fed5ae80c19b700dd0
Divyabhandari-06 <dpbhandari.16@gmail.com>
2025-03-26 10:05:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('healthForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const weight = document.getElementById('weight').value;
-    const height = document.getElementById('height').value;
-    alert(Your weight: ${weight}kg, height: ${height}cm);
-});
